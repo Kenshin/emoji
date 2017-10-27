f6d25cbcf0ce306533d3fc5f80feb05f6fd44b4c
Kenshin <fxblog@gmail.com>
2017-10-27 12:46:22 +0800

    :sparkles: Add options logic.

--- src/options/setting.jsx
+++ src/options/setting.jsx
@@ -1,36 +1,48 @@
 console.log( "=== +emoji options: setting load ===" )
 
-import Button     from 'button';
 import * as waves from 'waves';
 import * as tt    from 'tooltip';
+import Button     from 'button';
 import Switch     from 'switch';
 import TextField  from 'textfield';
 
 export default class Setting extends React.Component {
 
+    onChange( value, key ) {
+        this.props.options[key] = value;
+    }
+
+    onChangeBlacklist() {
+        this.props.options.blacklist = event.target.value.split( "\n" );
+    }
+
+    onClick( type ) {
+        console.log( this.props.options )
+    }
+
     render() {
         return (
             <div>
-                <Switch width="100%" checked={ false }
+                <Switch width="100%" checked={ this.props.options.blank }
                     thumbedColor="#94AC3C" trackedColor="#94AC3C" waves="md-waves-effect"
                     label="在复制或插入 Emoji 时是否前后增加一个空格？"
-                />
-                <Switch width="100%" checked={ false }
+                    onChange={ (s)=>this.onChange(s, "blank" ) } />
+                <Switch width="100%" checked={ this.props.options.clip }
                     thumbedColor="#94AC3C" trackedColor="#94AC3C" waves="md-waves-effect"
                     label="插入 Emoji 后是否也同时复制到剪切板？"
-                />
-                <Switch width="100%" checked={ false }
+                    onChange={ (s)=>this.onChange(s, "clip" ) } />
+                <Switch width="100%" checked={ this.props.options.clicked }
                     thumbedColor="#94AC3C" trackedColor="#94AC3C" waves="md-waves-effect"
                     label="Popup Tab 栏是否启用点击切换？（默认为鼠标悬停切换）"
-                />
+                    onChange={ (s)=>this.onChange(s, "clicked" ) } />
                 <TextField 
                     multi={ false }
                     placeholder="插入 Emoji 的触发条件默认为 中英文冒号 + 中英文关键字 + 空格，例如 ::face 或 ：：笑 "
                 />
-                <Switch width="100%" checked={ false }
+                <Switch width="100%" checked={ this.props.options.advanced }
                     thumbedColor="#94AC3C" trackedColor="#94AC3C" waves="md-waves-effect"
                     label="是否开启触发条件高级模式？（开启后，触发条件将会失效）"
-                />
+                    onChange={ (s)=>this.onChange(s, "advanced" ) } />
                 <TextField 
                     multi={ false }
                     placeholder="仅支持正则表达式。"
@@ -38,7 +50,18 @@ export default class Setting extends React.Component {
                 <div className="name">黑名单</div>
                 <TextField 
                     multi={ true } rows="5"
-                />
+                    value={ this.props.options.blacklist.replace( ",", "\n" ) }
+                    onChange={ ()=>this.onChangeBlacklist() } />
+                <Button type="raised" text="保存"
+                    style={{ "margin": "0" }}
+                    color="#fff" backgroundColor="#94AC3C"
+                    waves="md-waves-effect md-waves-button"
+                    onClick={ ()=>this.onClick( "save" ) } />
+                <Button type="raised" text="初始化"
+                    style={{ "margin": "10px 0 0" }}
+                    color="#fff" backgroundColor="#FF5252"
+                    waves="md-waves-effect md-waves-button"
+                    onClick={ ()=>this.onClick( "clear" ) } />
             </div>
         )
     }
