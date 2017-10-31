708a4c49ddd8f5958da9ef900b1ed19ce6f869b1
Kenshin <fxblog@gmail.com>
2017-10-31 15:32:04 +0800

    :art: Update fullpage options.

--- src/index.js
+++ src/index.js
@@ -9,7 +9,9 @@ $( document ).ready( function() {
     $( "#fullpage" ).fullpage({
         sectionsColor     : [ "#FFCA28", "#80DEEA", "#ff7281", "#64B5F6", "#c5e763" ],
         anchors           : [ "welcome", "popup", "window", "insert", "option", "footer" ],
+        loopBottom        : true,
         navigation        : true,
         navigationPosition: "right",
+        navigationTooltips: [ "下载", "快捷键", "独立窗口", "中文语义化", "定制化", "联系" ],
     });
 });
